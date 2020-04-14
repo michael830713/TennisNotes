c0da6f189259f1c88f8a33c50d0eda26507c2822
mike <michael830713@gmail.com>
2020-04-14 16:23:56 +0800

    made a custom toolbar

--- app/src/main/java/com/mike/tennisnotes/MainActivity.kt
+++ app/src/main/java/com/mike/tennisnotes/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.mike.tennisnotes
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,13 +20,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+//        val toolBar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
 
+//        topAppBar.setOnMenuItemClickListener { item ->
+//            when (item.itemId) {
+//
+//                R.id.more -> {
+//                    // Handle more item (inside overflow menu) press
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+        )
+
+        setSupportActionBar(toolbar)
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateView(
+        parent: View?,
+        name: String,
+        context: Context,
+        attrs: AttributeSet
+    ): View? {
+        return super.onCreateView(parent, name, context, attrs)
+
+
     }
 }
--- app/src/main/java/com/mike/tennisnotes/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/mike/tennisnotes/ui/dashboard/DashboardFragment.kt
@@ -1,31 +1,41 @@
 package com.mike.tennisnotes.ui.dashboard
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.mike.tennisnotes.R
+import kotlinx.android.synthetic.main.fragment_dashboard.view.*
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
+
+        setHasOptionsMenu(true)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+
+
+
     }
 }
