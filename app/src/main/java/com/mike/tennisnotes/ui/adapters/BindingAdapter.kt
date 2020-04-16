1bedeff8b69ae6f7cde06577c82cdd31a09fff8e
mike <michael830713@gmail.com>
2020-04-16 11:30:15 +0800

    added the cameraX permission and viewFinder and able to display the camera view

--- app/src/main/java/com/mike/tennisnotes/ui/adapters/BindingAdapter.kt
+++ app/src/main/java/com/mike/tennisnotes/ui/adapters/BindingAdapter.kt
@@ -1,13 +1,9 @@
 package com.mike.tennisnotes.ui.adapters
 
-import android.text.method.LinkMovementMethod
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 @BindingAdapter("imageFromUrl")
--- app/src/main/java/com/mike/tennisnotes/ui/dashboard/NewNoteFragment.kt
+++ app/src/main/java/com/mike/tennisnotes/ui/dashboard/NewNoteFragment.kt
@@ -1,17 +1,34 @@
 package com.mike.tennisnotes.ui.dashboard
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.graphics.Matrix
 import android.os.Bundle
+import android.util.Size
 import android.view.*
-import android.widget.TextView
+import android.widget.Toast
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.RecyclerView
 import com.mike.tennisnotes.R
+import java.util.concurrent.Executors
+
+
+private const val REQUEST_CODE_PERMISSIONS = 10
+
+// This is an array of all the permission specified in the manifest.
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
 class NewNoteFragment : Fragment() {
 
+
     private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var viewFinder: TextureView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,12 +38,111 @@ class NewNoteFragment : Fragment() {
         dashboardViewModel =
             ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_new_note, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
 
-
-
+        viewFinder = root.findViewById(R.id.view_finder)
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+        if (allPermissionsGranted()) {
+            viewFinder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                (context as Activity?)!!, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
 
         return root
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        // Request camera permissions
+
+
+        // Every time the provided texture view changes, recompute layout
+
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                Toast.makeText(
+                    context,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            (context as Activity?)!!, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private val executor = Executors.newSingleThreadExecutor()
+
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(640, 480))
+        }.build()
+
+
+        // Build the viewfinder use case
+        val preview = Preview(previewConfig)
+
+        // Every time the viewfinder is updated, recompute layout
+        preview.setOnPreviewOutputUpdateListener {
+
+            // To update the SurfaceTexture, we have to remove it and re-add it
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+        // Bind use cases to lifecycle
+        // If Android Studio complains about "this" being not a LifecycleOwner
+        // try rebuilding the project or updating the appcompat dependency to
+        // version 1.1.0 or higher.
+        CameraX.bindToLifecycle(this, preview)
+    }
+
+    private fun updateTransform() {
+
+        val matrix = Matrix()
+
+        // Compute the center of the view finder
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        // Correct preview output to account for display rotation
+        val rotationDegrees = when(viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        // Finally, apply transformations to our TextureView
+        viewFinder.setTransform(matrix)
+
+    }
+
 
 }
